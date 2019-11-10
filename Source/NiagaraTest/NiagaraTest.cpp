c74f2369b0b8204119f75529940c5e9115a663a2
esl000 <pgunuk@gmail.com>
2019-11-11 08:13:52 +0900

    테스트한 동영상과 프로젝트 파일 업로드

---
+++ Source/NiagaraTest/NiagaraTest.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "NiagaraTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NiagaraTest, "NiagaraTest" );
+ 
